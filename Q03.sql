ec66223f83ba35cbe0a6c0b3b26c2899629e05b5
Sveloz03 <velozsam@gmail.com>
2026-06-02 16:37:48 -0400

    Q03

---
+++ Q03.sql
@@ -0,0 +1,11 @@
+use northwind;
+
+#3. What are the order ids of the orders that ordered "Sasquatch Ale"?  HINT:   Find 
+#	the product id of "Sasquatch Ale" in a subquery and then use that value to find 
+#	the matching orders from the `order details` table.  Because the `order details` 
+#	table has a space in its name, you will need to surround it with back ticks in the 
+#	FROM clause. 
+
+SELECT OrderID FROM `order details`
+WHERE ProductID = (SELECT ProductID FROM products
+					WHERE ProductName LIKE "%Sasquatch Ale%")
